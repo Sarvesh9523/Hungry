49bb4fadc8f4cb6ea820b82b7f16d97f153f1431
Sarvesh9523 <sarveshkumar9176@gmail.com>
2025-09-11 20:03:54 +0530

    22

--- Frontend/src/pages/food-partner/Profile.jsx
+++ Frontend/src/pages/food-partner/Profile.jsx
@@ -73,6 +73,7 @@ const FoodPartnerProfile = () => {
                     style={{ objectFit: "cover", width: "100%", height: "100%" }}
                     src={v.video}
                     muted
+                    loop
                   />
                 </div>
               ))
