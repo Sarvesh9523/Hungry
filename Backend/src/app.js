6022fb2b64e367351db3b4dbde805869646e7dca
Sarvesh9523 <sarveshkumar9176@gmail.com>
2026-04-14 23:13:58 +0530

    frontend link is updated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: ["https://hungry-peeps.vercel.app", "http://localhost:5173"], // remove trailing slash
+    origin: "https://hungry-peeps.vercel.app", // remove trailing slash
     credentials: true
 }));
 
